ebb960c86693c10ab5f17196d7cc096df9218168
Leonardo Nascimento <omgleo@live.com>
2023-02-08 23:53:32 -0300

    Usuario Inserir

--- src/main/java/com/projeto/web/controller/UsuarioController.java
+++ src/main/java/com/projeto/web/controller/UsuarioController.java
@@ -1,13 +1,17 @@
 package com.projeto.web.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projeto.web.model.Usuario;
 import com.projeto.web.service.UsuarioService;
@@ -30,6 +34,16 @@ public class UsuarioController {
     public ResponseEntity<Usuario>findByid(@PathVariable Long id){
         Usuario obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Usuario>inserir(@RequestBody Usuario obj){
+        obj = service.insert(obj);
+        URI  uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}")
+              .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
 
     }
 
--- src/main/java/com/projeto/web/service/UsuarioService.java
+++ src/main/java/com/projeto/web/service/UsuarioService.java
@@ -28,4 +28,8 @@ public class UsuarioService {
      
     }
 
+    public Usuario insert(Usuario obj){
+        return repository.save(obj);
+    }
+
 }
