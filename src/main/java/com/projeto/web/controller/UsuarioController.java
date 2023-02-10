e684b610219d35c3fd255fdfc3f840007ba959fa
Leonardo Nascimento <omgleo@live.com>
2023-02-09 22:23:08 -0300

    Usuario update

--- src/main/java/com/projeto/web/controller/UsuarioController.java
+++ src/main/java/com/projeto/web/controller/UsuarioController.java
@@ -4,11 +4,13 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +54,15 @@ public class UsuarioController {
     public ResponseEntity<Void>delete(@PathVariable Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
+
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<Usuario>update(@PathVariable Long id , @RequestBody Usuario obj){
+
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);                                   
 
     }
 
